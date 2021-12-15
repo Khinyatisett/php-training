1874501fe1961eea99a7637a82633112b627e2ca
Khin yati Sett <khinyatisett5@gmail.com>
2021-12-15 16:23:12 +0630

    [main] updated code format

--- tutorials/tutorial_02/index.php
+++ tutorials/tutorial_02/index.php
@@ -1,37 +1,37 @@
 <?php
+$val=6;
+	for ($i = 1; $i <= $val; $i++)	
+	{
+		for($k = 6; $k > $i; $k-- )
+			{
+				echo "&nbsp &nbsp";
+			}
+		for($j = 1; $j <= $i; $j++ )
+			{
+				echo "* ";
+			}
+		for($j = $i-1; $j >= 1; $j-- )
+			{
+				echo" *";
+			}
+		echo "</br>";
+	}
 
-for ($i = 1; $i <= 6; $i++)	
-{
-for($k = 6; $k > $i; $k-- )
-{
-	echo "&nbsp &nbsp";
-}
-for($j = 1; $j <= $i; $j++ )
-{
-echo "* ";
-}
-for($j = $i-1; $j >= 1; $j-- )
-{
-echo" *";
-}
-echo "</br>";
-}
-
-for ($i = 5; $i >=1; $i--)	
-{
-for($k = 5; $k >= $i; $k-- )
-{
-	echo "&nbsp &nbsp";
-}
-for($j = 1; $j <= $i; $j++ )
-{
-echo "* ";
-}
-for($j = $i-1; $j >= 1; $j-- )
-{
-echo" *";
-}
-echo "</br>";
-}
+	for ($i = 5; $i >=1; $i--)	
+	{
+		for($k = 5; $k >= $i; $k-- )
+			{
+				echo "&nbsp &nbsp";
+			}
+		for($j = 1; $j <= $i; $j++ )
+			{
+				echo "* ";
+			}
+		for($j = $i-1; $j >= 1; $j-- )
+			{
+				echo" *";
+			}
+		echo "</br>";
+	}
 
 ?>
