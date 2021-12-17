fbafafa07622c76358552a98e18fb0257c5cf02b
Khin yati Sett <khinyatisett5@gmail.com>
2021-12-17 14:27:34 +0630

    [main] added xlxs library

---
+++ tutorials/tutorial_05/vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => '5109f3c536c525487afd3e7ee02aa2fa9a587362',
+        'name' => '__root__',
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => '5109f3c536c525487afd3e7ee02aa2fa9a587362',
+            'dev_requirement' => false,
+        ),
+        'aspera/xlsx-reader' => array(
+            'pretty_version' => 'v0.10.1',
+            'version' => '0.10.1.0',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../aspera/xlsx-reader',
+            'aliases' => array(),
+            'reference' => '2056b435bf186ecdea75db46bd1f3f00fa064a06',
+            'dev_requirement' => false,
+        ),
+    ),
+);
