fb55a7f95c0aa9efaa3c3d952b6f9647091fea5e
Khin yati Sett <khinyatisett5@gmail.com>
2021-12-17 11:12:04 +0630

    [main] finished show content by reading csv

---
+++ tutorials/tutorial_05/index.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <link rel="stylesheet" href="../tutorial_05/style.css">
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>tutorial_05</title>
+</head>
+<body>
+
+<?php
+	$csv_fp = fopen("sample.csv", "r");
+	if ($csv_fp !== FALSE) {
+?>
+  <h3>(1) content of csv file</h3>
+  <table id="table">
+  <tr>
+    <td>NAME</td>
+    <td>GENDER</td>
+  </tr>
+<?php	
+	while (! feof($csv_fp)) {
+	$data = fgetcsv($csv_fp, 1000, ",");
+?>
+  <tr class="data">
+  <td><?php echo $data[0]; ?></td>
+  <td><?php echo $data[1]; ?></td>
+  </tr>
+<?php
+	}
+?>
+  </table>
+<?php	
+	}
+	fclose($csv_fp);
+?>
+</body>
+</html>
