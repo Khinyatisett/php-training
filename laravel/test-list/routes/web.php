90bade4de4c02fd22971a3060d88dd73729a5cd9
Khin yati Sett <khinyatisett5@gmail.com>
2021-12-28 15:32:31 +0630

    [main] finished testlist

---
+++ laravel/test-list/routes/web.php
@@ -0,0 +1,60 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+use Illuminate\Support\Facades\Validator;
+use App\Models\Task;
+
+use Illuminate\Http\Request;
+
+/**
+ * Display All Tasks
+ */
+Route::get('/', function () {
+    Route::get('/', function () {
+        $tasks = Task::all();
+        dd($tasks);
+        return view('tasks', [
+            'tasks' => $tasks
+        ]);
+    });
+   return view('tasks');
+});
+
+/**
+ * Add A New Task
+ */
+Route::post('/task', function (Request $request) {
+    $validator = Validator::make($request->all(), [
+        'name' => 'required|max:255',
+    ]);
+    if ($validator->fails()) {
+        return redirect('/')
+            ->withInput()
+            ->withErrors($validator);
+    }
+    $task = new Task;
+    $task->name = $request->name;
+    $task->save();
+
+    return redirect('/');
+});
+
+/**
+ * Delete An Existing Task
+ */
+Route::delete('/task/{id}', function ($id) {
+    Task::findOrFail($id)->delete();
+
+    return redirect('/');
+});
